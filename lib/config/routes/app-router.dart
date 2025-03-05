3f99c632bd4e85e866ea4ebb0703101c3cac694b
t1asegovia <asegovia@todo1.net>
2025-03-05 10:55:40 -0500

    go router name

--- lib/config/routes/app-router.dart
+++ lib/config/routes/app-router.dart
@@ -4,14 +4,17 @@ import 'package:pelis_app/presentation/screens/screens.dart';
 final router = GoRouter(
   routes: [
     GoRoute(
+      name: HomeScreen.name,
       path: '/',
       builder: (context, state) => const HomeScreen(),
     ),
     GoRoute(
+      name: ButtonsScreen.name,
       path: '/buttons',
       builder: (context, state) => const ButtonsScreen(),
     ),
     GoRoute(
+      name: CardsScreen.name,
       path: '/cards',
       builder: (context, state) => const CardsScreen(),
     ),
