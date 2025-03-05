5dbcd0cb43c690d1f4d2b0dffdbf94308c6440fc
t1asegovia <asegovia@todo1.net>
2025-03-05 10:13:30 -0500

    list view

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pelis_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,7 +10,46 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("title buttons"),
       ),
-      body: null,
+      body: _HomeView(),
     );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItem.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItem[index];
+
+        return _CustomListTile(menuItem: menuItem);
+      }
+      );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItems menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        print("·hola");
+      },
+      );
   }
 }
