cb25051361074ae4fa08d0836a0b9d3f65c05118
t1asegovia <asegovia@todo1.net>
2025-03-06 09:13:38 -0500

    news screens

---
+++ lib/presentation/screens/animated/animated_screen.dart.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AnimatedScreenDart extends StatelessWidget {
+
+  const AnimatedScreenDart({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           body: Container(),
+       );
+  }
+}
---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScreen extends StatelessWidget {
+
+  const AppTutorialScreen({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           body: Container(),
+       );
+  }
+}
