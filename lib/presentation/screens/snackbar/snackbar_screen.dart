9582700d8e02b7d23934c66715392072cdb65a67
t1asegovia <asegovia@todo1.net>
2025-03-06 11:02:42 -0500

    add snacbar

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
 class SnackbarScreen extends StatelessWidget {
+  const SnackbarScreen({super.key});
 
-  const SnackbarScreen({ super.key });
+  void showSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+    final snackbar = SnackBar(
+      content: Text('hola'),
+      action: SnackBarAction(label: 'Ok', onPressed: () {},),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars and dialogs'),
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('mostrar snack'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showSnackBar(context),
+      ),
+    );
   }
 }
