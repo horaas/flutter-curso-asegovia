1f35c61738ba340e903af57802af5e542c492f81
horacio <shoracioa@gmail.com>
2025-03-08 21:39:29 -0500

    add reiver pod

---
+++ cinemapedia/lib/infraestructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,32 @@
+import 'package:cinemapedia/configs/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasources.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infraestructure/models/moviedb/moviedb_response.dart';
+import 'package:dio/dio.dart';
+
+class MoviedbDatasource extends MoviesDatasources {
+  final _dio = Dio(
+    BaseOptions(
+      baseUrl: Environment.movieUrl,
+      queryParameters: {
+        'api_key': Environment.theMovieDvKey,
+        'language': 'es-MX',
+      },
+    ),
+  );
+
+  MoviedbDatasource();
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await _dio.get('/movie/now_playing');
+    final movieDBREsponse = MovieDbRespopnse.fromJson(response.data);
+    final List<Movie> movies =
+        movieDBREsponse.results
+            .where((movieDB) => movieDB.posterPath != 'no-poster')
+            .map((movieDB) => MovieMapper.movieDBToEntity(movieDB))
+            .toList();
+    return movies;
+  }
+}
