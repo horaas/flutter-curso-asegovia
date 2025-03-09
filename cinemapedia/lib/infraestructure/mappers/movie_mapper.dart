e59f94a79ee2ac82314231fd7da0d1661fb1c512
horacio <shoracioa@gmail.com>
2025-03-08 21:40:10 -0500

    mapper el que mapea los datos

---
+++ cinemapedia/lib/infraestructure/mappers/movie_mapper.dart
@@ -0,0 +1,29 @@
+import 'package:cinemapedia/configs/constants/environment.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/models/moviedb/movie_movieDB.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MoviMovieDB moviedb) => Movie(
+    adult: moviedb.adult,
+    backdropPath:
+        moviedb.backdropPath != ''
+            ? Environment.urlImageMovie + moviedb.backdropPath
+            : 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg',
+    genreIds:
+        moviedb.genreIds.map((toElement) => toElement.toString()).toList(),
+    id: moviedb.id,
+    originalLanguage: moviedb.originalLanguage,
+    originalTitle: moviedb.originalTitle,
+    overview: moviedb.overview,
+    popularity: moviedb.popularity,
+    posterPath:
+        moviedb.posterPath != ''
+            ? Environment.urlImageMovie + moviedb.posterPath
+            : 'no-poster',
+    releaseDate: moviedb.releaseDate,
+    title: moviedb.title,
+    video: moviedb.video,
+    voteAverage: moviedb.voteAverage,
+    voteCount: moviedb.voteCount,
+  );
+}
