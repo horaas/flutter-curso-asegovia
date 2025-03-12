7dddecf103db52f64ccb484ac6b4e9055a525581
horacio <shoracioa@gmail.com>
2025-03-11 21:32:11 -0500

    add comfig errors

--- cinemapedia/lib/infraestructure/datasources/actor_moviedb_datsources.dart
+++ cinemapedia/lib/infraestructure/datasources/actor_moviedb_datsources.dart
@@ -17,7 +17,7 @@ class ActorMoviedbDatsources extends ActorDatsources {
 
   @override
   Future<List<Actor>> getActorByMovie(String movieId) async {
-    final response = await _dio.get('movie/$movieId/credits');
+    final response = await _dio.get('/movie/$movieId/credits');
     final actorResponse = CreditsResponse.fromJson(response.data);
     return actorResponse.cast
         .map((cast) => ActorMapper.castToEntity(cast))
