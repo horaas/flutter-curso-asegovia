e59766fbbb4095fab00984e4e7c7e552c9996945
horacio <shoracioa@gmail.com>
2025-03-13 16:47:58 -0500

    add insatance database open

--- cinemapedia/lib/infraestructure/datasources/isar_datasource.dart
+++ cinemapedia/lib/infraestructure/datasources/isar_datasource.dart
@@ -1,15 +1,24 @@
 import 'package:cinemapedia/domain/domains.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasource extends LocalStorageDatasource {
+  late Future<Isar> db;
+  IsarDatasource() {
+    db: openDB;
+  }
+
+  Future<Isar> openDB() async {
+    final directory = await getApplicationDocumentsDirectory();
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema], inspector: true, directory: directory.path);
+    }
+    return Future.value(Isar.getInstance());
+  }
+  
   @override
   Future<bool> isMovieFAvorite(int movieId) {
     // TODO: implement isMovieFAvorite
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
     throw UnimplementedError();
   }
 
@@ -19,4 +28,9 @@ class IsarDatasource extends LocalStorageDatasource {
     throw UnimplementedError();
   }
 
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    // TODO: implement loadMovies
+    throw UnimplementedError();
+  }
 }
