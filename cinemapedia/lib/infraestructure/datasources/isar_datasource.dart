3d32db8aa874aeac099b75f75f004bb21d0c2c58
horacio <shoracioa@gmail.com>
2025-03-13 17:06:46 -0500

    add datasource implementation

--- cinemapedia/lib/infraestructure/datasources/isar_datasource.dart
+++ cinemapedia/lib/infraestructure/datasources/isar_datasource.dart
@@ -5,32 +5,52 @@ import 'package:path_provider/path_provider.dart';
 class IsarDatasource extends LocalStorageDatasource {
   late Future<Isar> db;
   IsarDatasource() {
-    db: openDB;
+    db:
+    openDB;
   }
 
   Future<Isar> openDB() async {
     final directory = await getApplicationDocumentsDirectory();
     if (Isar.instanceNames.isEmpty) {
-      return await Isar.open([MovieSchema], inspector: true, directory: directory.path);
+      return await Isar.open(
+        [MovieSchema],
+        inspector: true,
+        directory: directory.path,
+      );
     }
     return Future.value(Isar.getInstance());
   }
-  
+
   @override
-  Future<bool> isMovieFAvorite(int movieId) {
-    // TODO: implement isMovieFAvorite
-    throw UnimplementedError();
+  Future<bool> isMovieFAvorite(int movieId) async {
+    final isar = await db;
+
+    final Movie? isFAvoriteMovie =
+        await isar.movies.filter().idEqualTo(movieId).findFirst();
+
+    return isFAvoriteMovie != null;
   }
 
   @override
-  Future<void> toogleFavorites(Movie movie) {
-    // TODO: implement toogleFavorites
-    throw UnimplementedError();
+  Future<void> toogleFavorites(Movie movie) async {
+    final isar = await db;
+    final favoriteMovie =
+        await isar.movies.filter().idEqualTo(movie.id).findFirst();
+
+    if (favoriteMovie != null) {
+      
+      isar.writeTxnSync(() => isar.movies.delete(favoriteMovie.idIsar!),);
+      return;
+      
+    }
+
+    isar.writeTxnSync(() => isar.movies.putSync(movie),);
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
-    throw UnimplementedError();
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    final isar = await db;
+
+    return isar.movies.where().offset(offset).limit(limit).findAll();
   }
 }
