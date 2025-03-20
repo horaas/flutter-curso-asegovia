6ec6c348e87ba3aa981e26d080ec739224e180f5
horacio <shoracioa@gmail.com>
2025-03-20 08:31:00 -0500

    variables de entorno

---
+++ store-app/lib/config/constants/environment.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Environment {
+  static initEnviroments() async {
+    await dotenv.load(fileName: '.env');
+  }
+  static String apiUrl = dotenv.env["API_URL"] ?? 'error de url';
+}
