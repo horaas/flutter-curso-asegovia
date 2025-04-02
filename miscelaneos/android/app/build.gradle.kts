4a47e4bb06a69add3ea9dc11e923134a59765363
horacio <shoracioa@gmail.com>
2025-04-02 13:58:46 -0500

    GRADLE

--- miscelaneos/android/app/build.gradle.kts
+++ miscelaneos/android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "ccom.mynatillera.app"
-    compileSdk = 34
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
