c2638e532b59a8de482e120c05f571b42e91973d
horacio <shoracioa@gmail.com>
2025-04-09 19:54:06 -0500

    dost desing

--- disenos_app/lib/presentation/slide_shows_screen.dart
+++ disenos_app/lib/presentation/slide_shows_screen.dart
@@ -9,9 +9,53 @@ class SlideShowsScreen extends StatelessWidget {
    Widget build(BuildContext context) {
        return Scaffold(
            body: Center(
-            child: _Slides(),
+            child: Column(
+              children: [
+                Expanded(child: _Slides()),
+                _Dots()
+              ],
+            ),
            ),
        );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  const _Dots();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Dot(),
+          _Dot(),
+          _Dot(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  const _Dot({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12,
+      height: 12,
+      margin: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        shape: BoxShape.circle
+      ),
+    );
   }
 }
 
