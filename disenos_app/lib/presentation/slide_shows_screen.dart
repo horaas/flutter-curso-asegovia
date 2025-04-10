e01583d67bd1e5e350438bac74f97cb402319268
horacio <shoracioa@gmail.com>
2025-04-09 19:45:45 -0500

    add slide configuration

--- disenos_app/lib/presentation/slide_shows_screen.dart
+++ disenos_app/lib/presentation/slide_shows_screen.dart
@@ -9,8 +9,40 @@ class SlideShowsScreen extends StatelessWidget {
    Widget build(BuildContext context) {
        return Scaffold(
            body: Center(
-            child: SvgPicture.asset('assets/images/svgs/slides/slide-1.svg'),
+            child: _Slides(),
            ),
        );
   }
 }
+
+class _Slides extends StatelessWidget {
+  const _Slides();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        _Slide('assets/images/svgs/slides/slide-1.svg'),
+        _Slide('assets/images/svgs/slides/slide-2.svg'),
+        _Slide('assets/images/svgs/slides/slide-3.svg'),
+      ],
+    );
+    
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  const _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      padding: const EdgeInsets.all(30),
+      child: SvgPicture.asset(svg));
+  }
+}
+
