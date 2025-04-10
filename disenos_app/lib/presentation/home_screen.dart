b28506593b7cc73fc755b9c1f51e379cdff3d634
horacio <shoracioa@gmail.com>
2025-04-09 19:35:28 -0500

    add pages slide

--- disenos_app/lib/presentation/home_screen.dart
+++ disenos_app/lib/presentation/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:disenos_app/presentation/animations_screen.dart';
 import 'package:disenos_app/presentation/circular_progress_indicator_screen.dart';
 import 'package:disenos_app/presentation/graficars_circulares_screen.dart';
+import 'package:disenos_app/presentation/slide_shows_screen.dart';
 import 'package:disenos_app/retos/square_animated.dart';
 // import 'package:disenos_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,7 @@ class HomeScreen extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return const Scaffold(
-           body: GraficarsCircularesScreen(),
+           body: SlideShowsScreen(),
        );
   }
 }
---
+++ disenos_app/lib/presentation/slide_shows_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SlideShowsScreen extends StatelessWidget {
+
+  const SlideShowsScreen({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           body: Center(
+            child: Text('data'),
+           ),
+       );
+  }
+}
