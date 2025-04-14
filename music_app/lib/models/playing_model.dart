d522ea49cb8a3a71e8d098626364bfdf3a8a4427
horacio <shoracioa@gmail.com>
2025-04-14 10:12:54 -0500

    add methods for model

--- music_app/lib/models/playing_model.dart
+++ music_app/lib/models/playing_model.dart
@@ -3,11 +3,21 @@ import 'package:flutter/material.dart';
 class PlayingModel with ChangeNotifier {
   bool _isPlaying = false;
   late AnimationController _controller;
+  late Duration _current = const Duration(milliseconds: 0);
+  late Duration _songDuration = const Duration(milliseconds: 0);
+
+
+
+  String get songTotalDuration => printDuration(_songDuration);
+  String get currentSecond =>  printDuration(_current);
+
+  double get porcentaje => (_songDuration.inSeconds > 0) ? _current.inSeconds / _songDuration.inSeconds : 0;
+
 
   bool isPlaying() => _isPlaying;
 
-  set setIsPlaying(bool otption) {
-    _isPlaying = otption;
+  set setIsPlaying(bool value) {
+    _isPlaying = value;
     notifyListeners();
   }
 
@@ -17,5 +27,27 @@ class PlayingModel with ChangeNotifier {
     _controller = controller;
   }
 
+  Duration current() => _current;
 
+  set setcurrent(Duration value) {
+    _current = value;
+    notifyListeners();
+  }
+  Duration songDuration() => _songDuration;
+
+  set setsongDuration(Duration value) {
+    _songDuration = value;
+    notifyListeners();
+  }
+
+
+  String printDuration(Duration duration) {
+    String twoDigits(int n) {
+      if (n >= 10) return '$n';
+      return '0$n';
+    }
+    String twoDigitsMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitsSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return '$twoDigitsMinutes:$twoDigitsSeconds';
+  }
 }
