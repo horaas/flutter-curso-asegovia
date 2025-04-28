92cbb9572f20061f642842414f578d4aec82e43d
horacio <shoracioa@gmail.com>
2025-04-28 16:16:37 -0500

    add pages

--- real_time_chat_app/lib/config/routes.dart
+++ real_time_chat_app/lib/config/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:real_time_chat_app/presentation/register/register_screen.dart';
 import 'package:real_time_chat_app/presentation/screens/login/login_screen.dart';
+import 'package:real_time_chat_app/presentation/users/users_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
-  'login': (_) => LoginScreen(),
-  'register': (_) => RegisterScreen()
+  'login': (_) => const LoginScreen(),
+  'register': (_) => const RegisterScreen(),
+  'users': (_) => const UsersScreen(),
 };
