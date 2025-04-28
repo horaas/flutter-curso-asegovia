dda96418f5902d5e4d5eedfd3c86b83d6716175f
horacio <shoracioa@gmail.com>
2025-04-28 16:45:56 -0500

    add list tile

---
+++ real_time_chat_app/lib/models/user_model.dart
@@ -0,0 +1,10 @@
+
+class UserModel {
+  final bool online;
+  final String uuid;
+  final String nombre;
+  final String email;
+
+  UserModel({required this.online, required this.uuid, required this.nombre, required this.email});
+   
+}
--- real_time_chat_app/lib/presentation/users/users_screen.dart
+++ real_time_chat_app/lib/presentation/users/users_screen.dart
@@ -1,16 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:real_time_chat_app/models/user_model.dart';
 
-
-class UsersScreen extends StatelessWidget {
+class UsersScreen extends StatefulWidget {
   const UsersScreen({super.key});
 
+  @override
+  State<UsersScreen> createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
+  final List<UserModel> users = [
+    UserModel(online: true, uuid: '1', nombre: 'Juan', email: 'email'),
+    UserModel(online: false, uuid: '2', nombre: 'pedro', email: 'email'),
+    UserModel(online: true, uuid: '3', nombre: 'marmol', email: 'email'),
+    UserModel(online: true, uuid: '1', nombre: 'Juan', email: 'email'),
+    UserModel(online: false, uuid: '2', nombre: 'pedro', email: 'email'),
+    UserModel(online: true, uuid: '3', nombre: 'marmol', email: 'email'),
+    UserModel(online: true, uuid: '1', nombre: 'Juan', email: 'email'),
+    UserModel(online: false, uuid: '2', nombre: 'pedro', email: 'email'),
+    UserModel(online: true, uuid: '3', nombre: 'marmol', email: 'email'),
+    UserModel(online: true, uuid: '1', nombre: 'Juan', email: 'email'),
+    UserModel(online: false, uuid: '2', nombre: 'pedro', email: 'email'),
+    UserModel(online: true, uuid: '3', nombre: 'marmol', email: 'email'),
+    UserModel(online: true, uuid: '1', nombre: 'Juan', email: 'email'),
+    UserModel(online: false, uuid: '2', nombre: 'pedro', email: 'email'),
+    UserModel(online: true, uuid: '3', nombre: 'marmol', email: 'email'),
+    UserModel(online: true, uuid: '1', nombre: 'Juan', email: 'email'),
+    UserModel(online: false, uuid: '2', nombre: 'pedro', email: 'email'),
+    UserModel(online: true, uuid: '3', nombre: 'marmol', email: 'email'),
+    UserModel(online: true, uuid: '1', nombre: 'Juan', email: 'email'),
+    UserModel(online: false, uuid: '2', nombre: 'pedro', email: 'email'),
+    UserModel(online: true, uuid: '3', nombre: 'marmol', email: 'email'),
+    UserModel(online: true, uuid: '1', nombre: 'Juan', email: 'email'),
+    UserModel(online: false, uuid: '2', nombre: 'pedro', email: 'email'),
+    UserModel(online: true, uuid: '3', nombre: 'marmol', email: 'email'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('UsuariosPage'),
-     ),
-   );
+      appBar: AppBar(
+        leading: const Icon(Icons.exit_to_app),
+        title: const Text('Name'),
+        centerTitle: true,
+        actions: [
+          const Padding(
+            padding: EdgeInsets.all(25),
+            child: Icon(Icons.check_circle, color: Colors.greenAccent),
+          ),
+        ],
+      ),
+      body: ListView.separated(
+        itemBuilder:
+            (_, index) => ListTile(
+              title: Text(users[index].nombre),
+              leading: CircleAvatar(
+                child: Text(users[index].nombre.substring(0, 2)),
+              ),
+              trailing: CircleAvatar(
+                child: Icon(
+                  Icons.circle,
+                  color: users[index].online ? Colors.green : Colors.redAccent,
+                ),
+              ),
+            ),
+        separatorBuilder: (_, index) => const Divider(),
+        itemCount: users.length,
+        physics: const BouncingScrollPhysics(),
+      ),
+    );
   }
 }
