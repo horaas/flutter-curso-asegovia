0a3be1271ea2fd298ba053dfe428804fe415f998
horacio <shoracioa@gmail.com>
2025-05-07 11:13:00 -0500

    fist pantalla maps

--- maps_app/lib/presentation/screens/gps_access/gps_access_screen.dart
+++ maps_app/lib/presentation/screens/gps_access/gps_access_screen.dart
@@ -6,8 +6,44 @@ class GpsAccessScreen extends StatelessWidget {
 
    @override
    Widget build(BuildContext context) {
-       return Scaffold(
-           body: Container(),
+       return const Scaffold(
+           body: Center(
+            child: _AccesButton(),
+           ),
        );
+  }
+}
+
+class _EnabledGpsMessage extends StatelessWidget {
+  const _EnabledGpsMessage();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('acceso concedido');
+  }
+}
+
+class _AccesButton extends StatelessWidget {
+  const _AccesButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Es necesario dar acceso al gps'),
+        MaterialButton(
+          color: Colors.black,
+          // https://api.flutter.dev/flutter/painting/ShapeBorder-class.html
+          // https://api.flutter.dev/flutter/painting/OutlinedBorder-class.html
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          onPressed: () {
+          
+        }, child: const Text('Dar Acceso', style: TextStyle(
+          color: Colors.white
+        ),),)
+      ],
+    );
   }
 }
