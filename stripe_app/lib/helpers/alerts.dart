07dfa5659d18fca172d06f381ce6fbfa3b40058b
horacio <shoracioa@gmail.com>
2025-05-15 10:05:06 -0500

    add poups

---
+++ stripe_app/lib/helpers/alerts.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+showLoading (BuildContext context) async {
+  showDialog(context: context, barrierDismissible: false, builder: (_) => const AlertDialog(
+      title: Text('Espere...'),
+      content: LinearProgressIndicator(),
+    )
+  );
+
+
+  await Future.delayed(const Duration(milliseconds: 2000));
+
+  if (context.mounted) {  
+    Navigator.of(context).pop();
+  }
+}
+showAlert(BuildContext context, String title, String message) async {
+  showDialog(context: context, barrierDismissible: false, builder: (_) => AlertDialog(
+      title: Text(title),
+      content: Text(message),
+      actions: [
+        MaterialButton(onPressed: () => Navigator.of(context).pop(),)
+      ],
+    )
+  );
+}
--- stripe_app/lib/helpers/helpers.dart
+++ stripe_app/lib/helpers/helpers.dart
@@ -1,1 +1,2 @@
 export './navigate_fadein.dart';
+export './alerts.dart';
